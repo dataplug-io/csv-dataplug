d5cda215ffeb5ef55c082f733fe943fee04a74bf
Alexey Pelykh <alexey.pelykh@gmail.com>
2017-11-03 07:54:00 +0200

    Test suite

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,3 +1,11 @@
 /* eslint-env node, mocha */
 require('chai')
   .should()
+const dataplugTestsuite = require('@dataplug/dataplug-testsuite')
+const csvDataplug = require('../lib')
+
+describe('toggl-dataplug', () => {
+  dataplugTestsuite
+    .forCollectionFactory('csv', csvDataplug)
+    .use()
+})
